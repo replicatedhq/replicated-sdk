4c910b90f6db223ce79acec02b9aaeafbfec0eb4
Salah Al Saleh <sg.alsaleh@gmail.com>
2023-04-10 12:03:23 -0700

    better license field signature structure (#9)

--- pkg/license/types/types.go
+++ pkg/license/types/types.go
@@ -1,13 +1,17 @@
 package types
 
 type LicenseField struct {
-	Name        string      `json:"name,omitempty"`
-	Title       string      `json:"title,omitempty"`
-	Description string      `json:"description,omitempty"`
-	Value       interface{} `json:"value,omitempty"`
-	ValueType   string      `json:"valueType,omitempty"`
-	IsHidden    bool        `json:"isHidden,omitempty"`
-	V1Signature []byte      `json:"v1Signature,omitempty"`
+	Name        string                `json:"name,omitempty"`
+	Title       string                `json:"title,omitempty"`
+	Description string                `json:"description,omitempty"`
+	Value       interface{}           `json:"value,omitempty"`
+	ValueType   string                `json:"valueType,omitempty"`
+	IsHidden    bool                  `json:"isHidden,omitempty"`
+	Signature   LicenseFieldSignature `json:"signature,omitempty"`
+}
+
+type LicenseFieldSignature struct {
+	V1 []byte `json:"v1,omitempty"`
 }
 
 type LicenseFields map[string]LicenseField
