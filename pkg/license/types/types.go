c2476c38243594d72a962c55646abc5c9c5b39e7
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2023-04-17 18:17:56 +0000

    add yaml tags

--- pkg/license/types/types.go
+++ pkg/license/types/types.go
@@ -1,17 +1,17 @@
 package types
 
 type LicenseField struct {
-	Name        string                `json:"name,omitempty"`
-	Title       string                `json:"title,omitempty"`
-	Description string                `json:"description,omitempty"`
-	Value       interface{}           `json:"value,omitempty"`
-	ValueType   string                `json:"valueType,omitempty"`
-	IsHidden    bool                  `json:"isHidden,omitempty"`
-	Signature   LicenseFieldSignature `json:"signature,omitempty"`
+	Name        string                `json:"name,omitempty" yaml:"name,omitempty"`
+	Title       string                `json:"title,omitempty" yaml:"title,omitempty"`
+	Description string                `json:"description,omitempty" yaml:"description,omitempty"`
+	Value       interface{}           `json:"value,omitempty" yaml:"value,omitempty"`
+	ValueType   string                `json:"valueType,omitempty" yaml:"valueType,omitempty"`
+	IsHidden    bool                  `json:"isHidden,omitempty" yaml:"isHidden,omitempty"`
+	Signature   LicenseFieldSignature `json:"signature,omitempty" yaml:"signature,omitempty"`
 }
 
 type LicenseFieldSignature struct {
-	V1 string `json:"v1,omitempty"` // this is a base64 encoded string because yaml.Unmarshal doesn't automatically convert base64 to []byte like json.Unmarshal does
+	V1 string `json:"v1,omitempty" yaml:"v1,omitempty"` // this is a base64 encoded string because yaml.Unmarshal doesn't automatically convert base64 to []byte like json.Unmarshal does
 }
 
 type LicenseFields map[string]LicenseField
