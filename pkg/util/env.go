8c70c20c27d22aa74a7db20c4d9ddf68f5b2d7fd
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2023-07-11 21:47:37 +0000

    Send reporting info in integration mode

--- pkg/util/env.go
+++ pkg/util/env.go
@@ -2,24 +2,8 @@ package util
 
 import (
 	"os"
-	"regexp"
-
-	kotsv1beta1 "github.com/replicatedhq/kots/kotskinds/apis/kots/v1beta1"
 )
 
 func IsAirgap() bool {
 	return os.Getenv("DISABLE_OUTBOUND_CONNECTIONS") == "true"
-}
-
-func IsDevEnv() bool {
-	return os.Getenv("REPLICATED_ENV") == "dev"
-}
-
-func IsDevLicense(license *kotsv1beta1.License) bool {
-	result, _ := regexp.MatchString(`replicated-app`, license.Spec.Endpoint)
-	return result
-}
-
-func IsIntegrationEnv() bool {
-	return os.Getenv("REPLICATED_INTEGRATION_LICENSE_ID") != ""
 }
