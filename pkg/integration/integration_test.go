5b6a629dd92511651bfb10690505cdc0a22cf0d2
Salah Aldeen Al Saleh <sg.alsaleh@gmail.com>
2023-07-14 20:20:31 +0000

    update tests

--- pkg/integration/integration_test.go
+++ pkg/integration/integration_test.go
@@ -113,6 +113,58 @@ func TestIntegration_IsEnabled(t *testing.T) {
 			want:    false,
 			wantErr: false,
 		},
+		{
+			name: "enabled for a dev license because key doesn't exist",
+			args: args{
+				clientset: fake.NewSimpleClientset(&corev1.SecretList{
+					TypeMeta: metav1.TypeMeta{},
+					ListMeta: metav1.ListMeta{},
+					Items: []corev1.Secret{{
+						TypeMeta: metav1.TypeMeta{},
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      replicatedSecretName,
+							Namespace: "default",
+						},
+						Data: map[string][]byte{},
+					}},
+				}),
+				namespace: "default",
+				license: &kotsv1beta1.License{
+					Spec: kotsv1beta1.LicenseSpec{
+						LicenseType: "dev",
+					},
+				},
+			},
+			want:    true,
+			wantErr: false,
+		},
+		{
+			name: "enabled for a dev license because value is empty",
+			args: args{
+				clientset: fake.NewSimpleClientset(&corev1.SecretList{
+					TypeMeta: metav1.TypeMeta{},
+					ListMeta: metav1.ListMeta{},
+					Items: []corev1.Secret{{
+						TypeMeta: metav1.TypeMeta{},
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      replicatedSecretName,
+							Namespace: "default",
+						},
+						Data: map[string][]byte{
+							replicatedIntegrationEnabledKey: []byte(""),
+						},
+					}},
+				}),
+				namespace: "default",
+				license: &kotsv1beta1.License{
+					Spec: kotsv1beta1.LicenseSpec{
+						LicenseType: "dev",
+					},
+				},
+			},
+			want:    true,
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
