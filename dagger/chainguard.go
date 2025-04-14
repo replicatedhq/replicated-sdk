eeeaf6a10123292031b7ac5d22c66ed1ba3c9a21
Marc Campbell <marc.e.campbell@gmail.com>
2025-04-14 15:33:52 -0700

    split to allow ttl.sh

--- dagger/chainguard.go
+++ dagger/chainguard.go
@@ -7,11 +7,16 @@ import (
 	"strings"
 )
 
-func buildAndPublishChainguardImage(ctx context.Context, dag *dagger.Client, source *dagger.Directory, version string) (string, error) {
+func buildAndPublishChainguardImage(
+	ctx context.Context,
+	dag *dagger.Client,
+	source *dagger.Directory,
+	version string,
+) (*dagger.Directory, *dagger.Directory, *dagger.File, error) {
 	// Update melange.yaml with correct version
 	melangeYaml, err := source.File("deploy/melange.yaml").Contents(ctx)
 	if err != nil {
-		return "", err
+		return nil, nil, nil, err
 	}
 	melangeYaml = strings.Replace(melangeYaml, "version: 1.0.0", fmt.Sprintf("version: %s", version), 1)
 	source = source.WithNewFile("deploy/melange.yaml", melangeYaml)
@@ -34,39 +39,38 @@ func buildAndPublishChainguardImage(ctx context.Context, dag *dagger.Client, sou
 	// Update apko.yaml with correct version
 	apkoYaml, err := source.File("deploy/apko.yaml").Contents(ctx)
 	if err != nil {
-		return "", err
+		return nil, nil, nil, err
 	}
 	apkoYaml = strings.Replace(apkoYaml, "VERSION: 1.0.0", fmt.Sprintf("VERSION: %s", version), 1)
 	source = source.WithNewFile("deploy/apko.yaml", apkoYaml)
 
-	// publish to docker hub (legacy, will be removed in the future)
-	image := dag.Apko().Publish(
+	return amdPackages, armPackages, melangeKey, nil
+}
+
+func publishChainguardImage(
+	ctx context.Context,
+	dag *dagger.Client,
+	source *dagger.Directory,
+	amdPackages *dagger.Directory,
+	armPackages *dagger.Directory,
+	melangeKey *dagger.File,
+	version string,
+	imagePath string,
+	username string,
+	password *dagger.Secret,
+) (string, error) {
+	image := dag.Apko(dagger.ApkoOpts{
+		RegistryUsername: username,
+		RegistryPassword: password,
+	}).Publish(
 		source.WithDirectory("packages", amdPackages).
 			WithDirectory("packages", armPackages).
 			WithFile("melange.rsa.pub", melangeKey),
 		source.File("deploy/apko.yaml"),
-		[]string{fmt.Sprintf("index.docker.io/replicated/replicated-sdk:%s", version)},
+		[]string{fmt.Sprintf("%s:%s", imagePath, version)},
 		dagger.ApkoPublishOpts{
 			Arch: "x86_64,aarch64",
 		},
-	).Container()
-
-	// publish to staging
-	dag.Apko().Publish(
-		source.WithDirectory("packages", amdPackages).
-			WithDirectory("packages", armPackages).
-			WithFile("melange.rsa.pub", melangeKey),
-		source.File("deploy/apko.yaml"),
-		[]string{fmt.Sprintf("registry.staging.replicated.com/library/replicated-sdk:%s", version)},
-	)
-
-	// publish to production
-	dag.Apko().Publish(
-		source.WithDirectory("packages", amdPackages).
-			WithDirectory("packages", armPackages).
-			WithFile("melange.rsa.pub", melangeKey),
-		source.File("deploy/apko.yaml"),
-		[]string{fmt.Sprintf("registry.replicated.com/library/replicated-sdk:%s", version)},
 	)
 
 	// return the image digest
--- dagger/publish.go
+++ dagger/publish.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"dagger/replicated-sdk/internal/dagger"
 	"fmt"
-	"os"
 )
 
 // Publish publishes the Replicated SDK images and chart
 // to staging and production registries
+//
+//	this is set up to default publish to ttl.sh and that's it
 func (m *ReplicatedSdk) Publish(
 	ctx context.Context,
 
@@ -19,20 +20,58 @@ func (m *ReplicatedSdk) Publish(
 
 	version string,
 
-	// +default=true
+	// +default=false
 	staging bool,
 
 	// +default=false
 	production bool,
+
+	// +default=true
+	dev bool,
+
+	// +default=false
+	slsa bool,
+
+	// +default="ttl.sh"
+	chartRegistry string,
+
+	// +optional
+	githubToken *dagger.Secret,
 ) error {
 	if err := generateReleaseNotesPR(ctx, source, opServiceAccount); err != nil {
 		return err
 	}
 
 	// version must be passed in, it will be used to tag the image
-	digest, err := buildAndPublishChainguardImage(ctx, dag, source, version)
+	amdPackages, armPackages, melangeKey, err := buildAndPublishChainguardImage(ctx, dag, source, version)
 	if err != nil {
 		return err
+	}
+
+	digest := ""
+	if dev {
+		digest, err = publishChainguardImage(ctx, dag, source, amdPackages, armPackages, melangeKey, version, "ttl.sh/replicated/replicated-sdk", "", nil)
+		if err != nil {
+			return err
+		}
+	}
+
+	if staging {
+		username := ""
+		var password *dagger.Secret
+		digest, err = publishChainguardImage(ctx, dag, source, amdPackages, armPackages, melangeKey, version, "registry.staging.replicated.com/library/replicated-sdk", username, password)
+		if err != nil {
+			return err
+		}
+	}
+
+	if production {
+		username := ""
+		var password *dagger.Secret
+		digest, err = publishChainguardImage(ctx, dag, source, amdPackages, armPackages, melangeKey, version, "registry.replicated.com/library/replicated-sdk", username, password)
+		if err != nil {
+			return err
+		}
 	}
 
 	err = buildAndPublishChart(ctx, dag, source, version, staging, production)
@@ -41,14 +80,13 @@ func (m *ReplicatedSdk) Publish(
 	}
 
 	// if we are running in CI we trigger the SLSA provenance workflow
-	if os.Getenv("CI") == "true" {
-		ghToken := dag.SetSecret("GITHUB_TOKEN", os.Getenv("GITHUB_TOKEN"))
+	if slsa {
 		ctr := dag.Gh().
 			Run(fmt.Sprintf(`api /repos/replicatedhq/replicated-sdk/actions/workflows/slsa.yml/dispatches \
 				-f ref=main \
 				-f inputs[digest]=%s`, digest),
 				dagger.GhRunOpts{
-					Token: ghToken,
+					Token: githubToken,
 				},
 			)
 		stdOut, err := ctr.Stdout(ctx)
