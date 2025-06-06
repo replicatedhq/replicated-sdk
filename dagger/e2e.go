b0732e2580f84a3b8924d779f2069c98d009c216
Andrew Lavery <laverya@umich.edu>
2025-06-06 13:06:55 -0400

    add TLS testing

--- dagger/e2e.go
+++ dagger/e2e.go
@@ -112,5 +112,65 @@ func e2e(
 
 	fmt.Println(out)
 
+	// create a tls cert and key
+	ctr = dag.Container().From("alpine/openssl:latest").
+		WithWorkdir("/certs").
+		WithExec([]string{"openssl", "req", "-x509", "-newkey", "rsa:4096", "-keyout", "certs/test-key.key", "-out", "certs/test-cert.crt", "-days", "365", "-nodes", "-subj", "/CN=test.com"})
+
+	// create a TLS secret within the namespace
+	ctr = dag.Container().From("bitnami/kubectl:latest").
+		WithFile("/root/.kube/config", kubeconfigSource.File("/kubeconfig")).
+		WithFile("/certs/test-cert.crt", source.File("/certs/test-cert.crt")).
+		WithFile("/certs/test-key.key", source.File("/certs/test-key.key")).
+		WithEnvVariable("KUBECONFIG", "/root/.kube/config").
+		WithExec(
+			[]string{
+				"kubectl", "create", "secret", "tls", "test-tls", "--cert", "/certs/test-cert.crt", "--key", "/certs/test-key.key",
+			})
+
+	// update the chart to set TLS to true
+	ctr = dag.Container().From("alpine/helm:latest").
+		WithFile("/root/.kube/config", kubeconfigSource.File("/kubeconfig")).
+		WithExec([]string{"helm", "upgrade", "test-chart", fmt.Sprintf("oci://registry.replicated.com/replicated-sdk-e2e/%s/test-chart", channelSlug), "--version", "0.1.0", "--set", "tlsCertSecretName=test-tls"})
+
+	out, err = ctr.Stdout(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to upgrade chart enabling TLS: %w", err)
+	}
+	fmt.Println(out)
+
+	// wait for the pod to be ready
+	ctr = dag.Container().From("bitnami/kubectl:latest").
+		WithFile("/root/.kube/config", kubeconfigSource.File("/kubeconfig")).
+		WithEnvVariable("KUBECONFIG", "/root/.kube/config").
+		WithExec(
+			[]string{
+				"kubectl", "wait",
+				"--for=condition=available",
+				"deployment/replicated",
+				"--timeout=1m",
+			})
+
+	out, err = ctr.Stdout(ctx)
+	if err != nil {
+		fmt.Printf("failed to wait for deployment to be ready after enabling TLS: %v\n", err)
+		// return err
+	}
+	fmt.Println(out)
+
+	// print the final pods
+	ctr = dag.Container().From("bitnami/kubectl:latest").
+		WithFile("/root/.kube/config", kubeconfigSource.File("/kubeconfig")).
+		WithEnvVariable("KUBECONFIG", "/root/.kube/config").
+		WithExec(
+			[]string{
+				"kubectl", "get", "pods",
+			})
+	out, err = ctr.Stdout(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get pods: %w", err)
+	}
+	fmt.Println(out)
+
 	return nil
 }
