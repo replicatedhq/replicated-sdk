5449a8639781750634bb4ab8e39ce927a0537f0d
Andrew Lavery <laverya@umich.edu>
2025-06-06 16:45:35 -0400

    bad deployment yaml test

--- dagger/e2e.go
+++ dagger/e2e.go
@@ -162,6 +162,7 @@ func e2e(
 	// create a kubernetes deployment that runs a pod - the pod has a readiness probe that runs 'curl -k https://replicated.svc:3000/health'
 	// this will only pass if the replicated pod is ready and serving TLS traffic
 	deploymentYaml := `
+	badyaml
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -196,10 +197,10 @@ spec:
 		WithFile("/root/replicated-ssl-test.yaml", deploymentSource.File("/replicated-ssl-test.yaml")).
 		WithExec([]string{"kubectl", "apply", "-f", "/root/replicated-ssl-test.yaml"})
 	out, err = ctr.Stdout(ctx)
-	fmt.Println(out)
 	if err != nil {
 		return fmt.Errorf("failed to apply replicated-ssl-test deployment: %w", err)
 	}
+	fmt.Println(out)
 
 	// wait for the replicated-ssl-test deployment to be ready
 	ctr = dag.Container().From("bitnami/kubectl:latest").
