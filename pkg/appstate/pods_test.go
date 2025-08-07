08c7c1391cb25b7502b32a952075a3400fe24029
Andrew Lavery <laverya@umich.edu>
2025-08-07 18:05:05 -0500

    ensure pod handler makes requests as expected

--- pkg/appstate/pods_test.go
+++ pkg/appstate/pods_test.go
@@ -3,8 +3,14 @@ package appstate
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	appstatetypes "github.com/replicatedhq/replicated-sdk/pkg/appstate/types"
+	"github.com/replicatedhq/replicated-sdk/pkg/store"
+	mock_store "github.com/replicatedhq/replicated-sdk/pkg/store/mock"
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8stypes "k8s.io/apimachinery/pkg/types"
 )
 
 func TestExtractImageInfo(t *testing.T) {
@@ -61,4 +67,79 @@ func TestExtractImageInfo(t *testing.T) {
 			assert.Equal(t, tt.expected, result)
 		})
 	}
+}
+
+func TestPodImageEventHandler_MockStoreCalls(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := mock_store.NewMockStore(ctrl)
+	store.SetStore(m)
+
+	ns := "ns"
+	handler := &podImageEventHandler{namespace: ns}
+
+	pod1UID := k8stypes.UID("pod-1-uid")
+	pod2UID := k8stypes.UID("pod-2-uid")
+
+	pod1Running := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod-1", UID: pod1UID},
+		Status: corev1.PodStatus{
+			Phase: corev1.PodRunning,
+			ContainerStatuses: []corev1.ContainerStatus{{
+				Image: "nginx@sha256:abcd1234567890abcd1234567890abcd1234567890abcd1234567890abcd1234",
+			}, {
+				Image: "redis@sha256:efgh5678901234efgh5678901234efgh5678901234efgh5678901234efgh5678",
+			}},
+			InitContainerStatuses: []corev1.ContainerStatus{{
+				Image: "busybox@sha256:ijkl9012345678ijkl9012345678ijkl9012345678ijkl9012345678ijkl9012",
+			}},
+		},
+	}
+	pod1Pending := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod-1", UID: pod1UID},
+		Status:     corev1.PodStatus{Phase: corev1.PodPending},
+	}
+	pod2Running := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "pod-2", UID: pod2UID},
+		Status: corev1.PodStatus{
+			Phase: corev1.PodRunning,
+			ContainerStatuses: []corev1.ContainerStatus{{
+				Image: "postgres@sha256:mnop3456789012mnop3456789012mnop3456789012mnop3456789012mnop3456",
+			}},
+		},
+	}
+
+	expectedPod1Images := []appstatetypes.ImageInfo{
+		{Name: "nginx", SHA: "sha256:abcd1234567890abcd1234567890abcd1234567890abcd1234567890abcd1234"},
+		{Name: "redis", SHA: "sha256:efgh5678901234efgh5678901234efgh5678901234efgh5678901234efgh5678"},
+		{Name: "busybox", SHA: "sha256:ijkl9012345678ijkl9012345678ijkl9012345678ijkl9012345678ijkl9012"},
+	}
+	expectedPod2Images := []appstatetypes.ImageInfo{
+		{Name: "postgres", SHA: "sha256:mnop3456789012mnop3456789012mnop3456789012mnop3456789012mnop3456"},
+	}
+
+	gomock.InOrder(
+		// Creating a running pod should set images
+		m.EXPECT().SetPodImages(ns, string(pod1UID), expectedPod1Images),
+		// Creating a pending pod should delete images
+		m.EXPECT().DeletePodImages(ns, string(pod1UID)),
+		// Updating to pending should delete images
+		m.EXPECT().DeletePodImages(ns, string(pod1UID)),
+		// Creating another running pod sets its images
+		m.EXPECT().SetPodImages(ns, string(pod2UID), expectedPod2Images),
+		// Updating back to running sets images again
+		m.EXPECT().SetPodImages(ns, string(pod1UID), expectedPod1Images),
+		// Deleting pod2 deletes its images
+		m.EXPECT().DeletePodImages(ns, string(pod2UID)),
+		// Deleting pod1 deletes its images
+		m.EXPECT().DeletePodImages(ns, string(pod1UID)),
+	)
+
+	handler.ObjectCreated(pod1Running)
+	handler.ObjectCreated(pod1Pending)
+	handler.ObjectUpdated(pod1Pending)
+	handler.ObjectCreated(pod2Running)
+	handler.ObjectUpdated(pod1Running)
+	handler.ObjectDeleted(pod2Running)
+	handler.ObjectDeleted(pod1Running)
 }
