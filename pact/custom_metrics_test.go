e5afc9f1abb39b6314b653c7de4e6c7dc28d56e5
alicenstar <67430892+alicenstar@users.noreply.github.com>
2026-03-31 15:34:05 -0500

    test(sc-135766): add Pact test for custom metrics in read-only mode

--- pact/custom_metrics_test.go
+++ pact/custom_metrics_test.go
@@ -98,3 +98,83 @@ func TestSendCustomAppMetrics(t *testing.T) {
 		}
 	})
 }
+
+func TestSendCustomAppMetrics_ReadOnlyMode(t *testing.T) {
+	// Happy path only - read-only mode
+
+	channelSequence := int64(1)
+	data := map[string]interface{}{
+		"data": map[string]interface{}{
+			"key1_string":         "val1",
+			"key2_int":            5,
+			"key3_float":          1.5,
+			"key4_numeric_string": "1.6",
+		},
+	}
+	customAppMetricsData, _ := json.Marshal(data)
+	license := &v1beta1.License{
+		Spec: v1beta1.LicenseSpec{
+			LicenseID: "replicated-sdk-license-customer-0-license",
+			AppSlug:   "replicated-sdk-license-app",
+			Endpoint:  fmt.Sprintf("http://%s:%d", pact.Host, pact.Server.Port),
+			ChannelID: "replicated-sdk-license-app-nightly",
+		},
+	}
+
+	clientWriter := httptest.NewRecorder()
+	clientRequest := &http.Request{
+		Body: io.NopCloser(bytes.NewBuffer(customAppMetricsData)),
+	}
+
+	pactInteraction := func() {
+		pact.
+			AddInteraction().
+			Given("Send valid custom app metrics in read-only mode").
+			UponReceiving("A request to send custom app metrics in read-only mode").
+			WithRequest(dsl.Request{
+				Method: http.MethodPost,
+				Headers: dsl.MapMatcher{
+					"User-Agent":                             dsl.String("Replicated-SDK/v0.0.0-unknown"),
+					"Authorization":                          dsl.String(fmt.Sprintf("Basic %s", base64.StdEncoding.EncodeToString([]byte(fmt.Sprintf("%s:%s", license.Spec.LicenseID, license.Spec.LicenseID))))),
+					"X-Replicated-DownstreamChannelID":       dsl.String(license.Spec.ChannelID),
+					"X-Replicated-DownstreamChannelSequence": dsl.String(fmt.Sprintf("%d", channelSequence)),
+				},
+				Path: dsl.String("/application/custom-metrics"),
+				Body: data,
+			}).
+			WillRespondWith(dsl.Response{
+				Status: http.StatusOK,
+			})
+	}
+	fakeClientSet := fake.NewSimpleClientset(
+		k8sutil.CreateTestDeployment(util.GetReplicatedDeploymentName(), "default", "1", map[string]string{"app": "replicated"}),
+		k8sutil.CreateTestReplicaSet("replicated-sdk-instance-replicaset", "default", "1"),
+		k8sutil.CreateTestPod("replicated-sdk-instance-pod", "default", "replicated-sdk-instance-replicaset", map[string]string{"app": "replicated"}),
+	)
+	t.Run("Send valid custom app metrics in read-only mode", func(t *testing.T) {
+		pactInteraction()
+
+		storeOptions := store.InitInMemoryStoreOptions{
+			License:               licensewrapper.LicenseWrapper{V1: license},
+			LicenseFields:         nil,
+			ReplicatedAppEndpoint: license.Spec.Endpoint,
+			ChannelID:             license.Spec.ChannelID,
+			ChannelSequence:       channelSequence,
+			Namespace:             "default",
+			ReadOnlyMode:          true,
+		}
+		store.InitInMemory(storeOptions)
+		defer store.SetStore(nil)
+
+		if err := pact.Verify(func() error {
+			handlers.SetTestClientSet(fakeClientSet)
+			handlers.SendCustomAppMetrics(clientWriter, clientRequest)
+			if clientWriter.Code != http.StatusOK {
+				return fmt.Errorf("expected status code %d but got %d", http.StatusOK, clientWriter.Code)
+			}
+			return nil
+		}); err != nil {
+			t.Fatalf("Error on Verify: %v", err)
+		}
+	})
+}
